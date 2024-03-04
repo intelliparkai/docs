c1de0ab384804a1de77e2abc2e3e22a464971e79
Muhammad Hanzla <wwhanzlah@gmail.com>
2024-03-04 19:27:27 +0500

    updated css + js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,13 @@ const date = new Date();
 const year = date.getFullYear();
 const yearSpan = document.querySelector("#year");
 yearSpan.innerText = year;
+
+
+const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+const screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+const title = document.querySelector("header h1");
+
+if (screenWidth <= 425){
+    title.innerHTML = "IntelliPark"
+}
